64395f8fee95549a28b9fad39eebfb7e85900d28
BSJ <9395987@ict-companion.com>
2022-08-01 17:15:13 +0900

    Pagelayout 개발시작

--- src/components/layout/PageLayout.js
+++ src/components/layout/PageLayout.js
@@ -1,33 +1,35 @@
-import { Container, Box, ButtonGroup, ListItemButton } from "@mui/material";
-import { values } from "mobx";
-import { LeftMenu } from "components";
-import { observer } from "mobx-react-lite";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
+import { Container, Box, ButtonGroup, ListItemButton } from '@mui/material';
+import { useLocation } from 'react-router-dom';
 
-// TODO : LeftMenu로 변경해야함
-import React from "react";
-import _ from "lodash";
-import { Scrollbars } from "react-custom-scrollbars-2";
+import { values } from 'mobx';
+import { LeftMenu } from 'components';
+import { observer } from 'mobx-react-lite';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+import React, { useEffect } from 'react';
+import _ from 'lodash';
+import { Scrollbars } from 'react-custom-scrollbars-2';
+import { useStore } from 'store';
 
 function PageLayout({ className }) {
+  const { pathname, key } = useLocation();
+  // viewStore 와 너무 어려운데
+  const { isLoading, viewStore } = useStore();
+  const { setCurrentMenu, currentMenu, codeList, menuList } = viewStore;
+
   return (
-    <Container
-      id="back-to-top-anchor"
-      maxWidth={false}
-      className={className}
-      sx={{ pt: "64px", height: "100%", minWidth: 1200, overflowY: "hidden" }}
-    >
-      <Box display="flex" sx={{ height: "100%" }}>
+    <Container id="back-to-top-anchor" maxWidth={false} className={className} sx={{ pt: '64px', height: '100%', minWidth: 1200, overflowY: 'hidden' }}>
+      <Box display="flex" sx={{ height: '100%' }}>
         {/* 좌측 메뉴 */}
         <LeftMenu />
         <Scrollbars>
           <Box
             component="main"
             sx={{
-              minHeight: "calc(100% - 51px)",
-              p: "10px 30px 100px",
-              bgcolor: "background.paper",
+              minHeight: 'calc(100% - 51px)',
+              p: '10px 30px 100px',
+              bgcolor: 'background.paper',
             }}
           ></Box>
         </Scrollbars>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,17 @@
-import { createContext, useContext } from "react";
-import _ from "lodash";
+import { createContext, useContext } from 'react';
+import _ from 'lodash';
 
 const StoreContext = createContext();
+
+// export function StoreProvider({ children }) {
+//   return <StoreContext.Provider value={store}>{children}</StoreContext.Provider>;
+// }
 
 export const useStore = (namespace) => {
   const store = useContext(StoreContext);
 
   if (namespace === undefined) {
-    throw Error("useStore must be used within StoreProvider.");
+    throw Error('useStore must be used within StoreProvider.');
   }
 
   if (_.isString(namespace)) {
