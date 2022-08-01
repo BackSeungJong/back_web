47aac960024db97eee8e65167607a21d5507b2f8
BSJ <9395987@ict-companion.com>
2022-08-01 10:55:02 +0900

    mui 적용

--- src/pages/main/View.js
+++ src/pages/main/View.js
@@ -1,5 +1,14 @@
+import Button from "@mui/material/Button";
+import BasicButtons from "../../components/input/Button";
+
 export const MainView = () => {
-  return <>Home</>;
+  return (
+    <>
+      Home
+      {/*<Button variant="contained">Home Button</Button>*/}
+      <BasicButtons />
+    </>
+  );
 };
 
 export default MainView;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 import { useRoutes } from "react-router-dom";
 
-import * as pages from "/src/pages";
+import * as pages from "pages";
 
 function Router() {
   return useRoutes([
     {
       element: <pages.MainView />,
-      children: [{ path: "/userMng/view", element: <pages.UserMngView /> }],
+      children: [
+        { path: "/", element: <pages.MainView /> },
+        { path: "/userMng/view", element: <pages.UserMngView /> },
+      ],
     },
   ]);
 }
